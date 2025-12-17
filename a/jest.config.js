eef033d73b4f5c80cfe5829b4adfe921ba8d4909
Ruben Khachatryan <ruben.khachatryan@aikido.dev>
2025-12-17 14:19:05 +0100

    more files

---
+++ a/jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    testEnvironment: 'node',
+    testMatch: ['**/tests/**/*.test.js'],
+    collectCoverageFrom: [
+        '*.js',
+        '!jest.config.js',
+        '!index.js'
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 70,
+            functions: 70,
+            lines: 70,
+            statements: 70
+        }
+    },
+    verbose: true,
+    testTimeout: 10000
+};
