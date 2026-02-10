f5ba041ba5a72342bf58dce9ac3ea19bf7201b3a
Rouben K <ruben.khachatryan@aikido.dev>
2026-02-10 22:20:23 +0400

    Update code.php

--- code.php
+++ code.php
@@ -1,10 +1,12 @@
 <?php
 if (isset($_GET['ns_dig']) && !empty($_GET['ns_dig'])) {
-$ns_dig = explode(',', $_GET['ns_dig']);
+    $ns_dig = explode(',', $_GET['ns_dig']);
 } else {
-$ns_dig = ['8.8.8.8', '8.8.4.4'];
+    $ns_dig = ['8.8.8.8', '8.8.4.4'];
 }
+
 $domains = ['domain' => 'example.com'];
+
 $first = `dig @$ns_dig[0] -t ns $domains[domain]`;
 echo "<pre>$first</pre>";
 
