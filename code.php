5a4a692307088ad7c57c26f715d66d4f85408dca
Rouben K <ruben.khachatryan@aikido.dev>
2026-02-10 20:15:02 +0400

    Create code.php

---
+++ code.php
@@ -0,0 +1,12 @@
+<?php
+if (isset($_GET['ns_dig']) && !empty($_GET['ns_dig'])) {
+$ns_dig = explode(',', $_GET['ns_dig']);
+} else {
+$ns_dig = ['8.8.8.8', '8.8.4.4'];
+}
+$domains = ['domain' => 'example.com'];
+$first = `dig @$ns_dig[0] -t ns $domains[domain]`;
+echo "<pre>$first</pre>";
+
+$second = `dig @$ns_dig[1] -t ns $domains[domain]`;
+echo "<pre>$second</pre>";
